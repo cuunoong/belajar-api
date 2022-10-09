bea67d9e9e7df9d25a4dd551527bde1c3321e53b
Arif Iskandar <arif19iskandar@gmail.com>
2022-10-09 23:47:21 +0700

    Secure router with verifyToken

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const verifyToken = require("../middlewares/verify-token");
 const router = express.Router();
 const usersHandler = require("./handlers/users");
 const usersIdHandler = require("./handlers/users/id");
 
-router.route("/").get(usersHandler.get).post(usersHandler.post);
+router.route("/").get(verifyToken, usersHandler.get);
 
 router
   .route("/:userId")
-  .get(usersIdHandler.get)
-  .put(usersIdHandler.put)
-  .delete(usersIdHandler.delete);
+  .get(verifyToken, usersIdHandler.get)
+  .put(verifyToken, usersIdHandler.put)
+  .delete(verifyToken, usersIdHandler.delete);
 
 module.exports = router;
